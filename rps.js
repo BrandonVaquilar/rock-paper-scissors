b850f883a307a9fdda2137194895f73b2cb8d6b5
BrandonVaquilar <68372469+BrandonVaquilar@users.noreply.github.com>
2025-10-29 21:05:16 +0000

    Complete Rock-Paper-Scissors

--- rps.js
+++ rps.js
@@ -1,1 +1,56 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let rock = "ROCK";
+    let paper = "PAPER";
+    let scissors = "SCISSORS";
+    let choice = undefined;
+    let random = Math.random();
+    if (random < 0.33) {
+        choice = rock;
+    }
+    else if (random < 0.66) {
+        choice = paper;
+    }
+    else {
+        choice = scissors;
+    }
+    return choice;
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper, or Scissors?");
+    choice = choice.toUpperCase();
+    if (choice !== "ROCK" && choice !== "PAPER" && choice !== "SCISSORS") {
+        console.log("Please enter a valid answer!");
+        return getHumanChoice();
+    }
+    return choice;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+        let winner = undefined;
+        
+        if (humanChoice === computerChoice) return `Tie! ${humanChoice} ties with ${computerChoice}!`;
+        if (humanChoice < computerChoice) {
+            humanScore += 1;
+            return `You win! ${humanChoice} beats ${computerChoice}!`;
+        } else {
+            computerScore += 1;
+            return `You lose! ${humanChoice} loses to ${computerChoice}!`;
+        }
+    }
+
+function playGame() {
+    //humanScore = 0;
+    //computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        console.log(playRound(getHumanChoice(), getComputerChoice()));
+    }
+    return console.log(`You Scored: ${humanScore}!\nComputer Scored: ${computerScore}!\nYou both tied ${5 - (humanScore + computerScore)} times!`);
+}
+
+playGame();
